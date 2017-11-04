18288cb1e336df58f68f1a986898fae3003a6bae
Richard Patterson <detobate@gmail.com>
2017-11-04 18:03:08 +0000

    moduling

---
+++ atlas_ipv6_opt/probes.py
@@ -0,0 +1,71 @@
+import bz2
+import ujson
+import collections
+import random
+import os
+from ripe.atlas.cousteau import (
+    Traceroute,
+    AtlasSource,
+    AtlasCreateRequest)
+
+probes_file = 'data/20171103.json.bz2'   # Probe list from http://ftp.ripe.net/ripe/atlas/probes/archive/meta-latest
+debug = True
+API_KEY = os.getenv('RIPE_API')
+
+tags = ('system-ipv6-works', 'system-ipv4-works', 'system-ipv4-stable-1d')
+
+
+def load_json():
+    probes = collections.defaultdict(list)
+    probes_blob = ujson.loads(bz2.BZ2File(probes_file).read())
+    for probe in probes_blob['objects']:
+        missing_tag = False
+        for tag in tags:
+            if tag not in probe['tags']:
+                missing_tag = True
+        if missing_tag:
+            continue
+        elif probe['is_public'] is True and probe['asn_v4'] == probe['asn_v6'] \
+                and probe['address_v4'] is not None and probe['address_v6'] is not None:
+            if len(probes[probe['asn_v6']]) < 1:
+                probes[probe['asn_v6']].append(probe)
+
+    if debug:
+        print("Found %s probes" % probes_blob['meta']['total_count'])
+        print("%s ASes with working dual stack" % len(probes))
+
+    return probes
+
+
+def create_measurements(probes, target):
+    num_probes = len(probes)
+
+    sources = AtlasSource(
+        type='probes',
+        value=','.join(str(x) for x in random.sample(probes, num_probes)),
+        requested=num_probes
+    )
+
+    tr4 = Traceroute(
+        af=4,
+        target=target['address_v4'],
+        description='Transient tr4 test',
+        protocol='ICMP'
+    )
+
+    tr6 = Traceroute(
+        af=6,
+        target=target['address_v6'],
+        description='Transient tr6 test',
+        protocol='ICMP'
+    )
+
+    atlas_request = AtlasCreateRequest(
+        key=API_KEY,
+        measurements=[tr4, tr6],
+        sources=[sources],
+        is_oneoff=True
+    )
+
+    response = atlas_request.create()
+    return response
--- main.py
+++ main.py
@@ -1,60 +1,19 @@
 #!/usr/bin/env python3
-from atlas_ipv6_opt.find_probes import load_json
+from atlas_ipv6_opt.probes import load_json, create_measurements
 import random
-import os
 import csv
 
-from ripe.atlas.cousteau import (
-    Traceroute,
-    AtlasSource,
-    AtlasCreateRequest)
-
-API_KEY = os.getenv('RIPE_API')
 base_url = 'https://atlas.ripe.net/api/v2/'
 probe_limit = 100
-debug = True
 output_csv = 'atlas_measurements.csv'
 interesting_asns = 'interesting_asns'
-
-
-def create_measurements(probes, target):
-    num_probes = len(probes)
-
-    sources = AtlasSource(
-        type='probes',
-        value=','.join(str(x) for x in random.sample(probes, num_probes)),
-        requested=num_probes
-    )
-
-    tr4 = Traceroute(
-        af=4,
-        target=target['address_v4'],
-        description='Transient tr4 test',
-        protocol='ICMP'
-    )
-
-    tr6 = Traceroute(
-        af=6,
-        target=target['address_v6'],
-        description='Transient tr6 test',
-        protocol='ICMP'
-    )
-
-    atlas_request = AtlasCreateRequest(
-        key=API_KEY,
-        measurements=[tr4, tr6],
-        sources=[sources],
-        is_oneoff=True
-    )
-
-    response = atlas_request.create()
-    return response
 
 
 def main():
 
     probes_by_as = load_json()
     probes = [p[0] for (a, p) in probes_by_as.items()]
+    # Hack to prioritise ASNs we think are problematic
     with open(interesting_asns) as r:
         asns = [int(l) for l in r.readlines()]
 
@@ -63,6 +22,7 @@ def main():
     for p in probes:
         if p['asn_v6'] in asns:
             new_probes[p['id']] = p
+    # Once we've selected the problematic ASNs, fill up remainder randomly
     for p in random.sample(probes, len(probes)):
         if len(new_probes) >= probe_limit:
             break
