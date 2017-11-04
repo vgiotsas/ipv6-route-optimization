74222485b68949644dd612f9e303ea29d2828be9
Richard Patterson <detobate@gmail.com>
2017-11-04 16:07:27 +0000

    All the measurements

--- atlas_ipv6_opt/find_probes.py
+++ atlas_ipv6_opt/find_probes.py
@@ -6,13 +6,20 @@ import collections
 probes_file = '20171103.json.bz2'
 debug = True
 
+tags = ('system-ipv6-works', 'system-ipv4-works', 'system-ipv4-stable-1d')
 
 def load_json():
     probes = collections.defaultdict(list)
     probes_blob = ujson.loads(bz2.BZ2File(probes_file).read())
     for probe in probes_blob['objects']:
-        if 'system-ipv6-works' in probe['tags'] and 'system-ipv4-works' in probe['tags'] \
-                and probe['asn_v4'] == probe['asn_v6'] and 'system-ipv4-stable-1d' in probe['tags']:
+        missing_tag = False
+        for tag in tags:
+            if tag not in probe['tags']:
+                missing_tag = True
+        if missing_tag:
+            continue
+        elif probe['is_public'] is True and probe['asn_v4'] == probe['asn_v6'] \
+                and probe['address_v4'] is not None and probe['address_v6'] is not None:
             if len(probes[probe['asn_v6']]) < 1:
                 probes[probe['asn_v6']].append(probe)
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from atlas_ipv6_opt.find_probes import load_json
 import random
 import os
+import csv
 
 from ripe.atlas.cousteau import (
     Traceroute,
@@ -9,9 +10,11 @@ from ripe.atlas.cousteau import (
     AtlasCreateRequest)
 
 API_KEY = os.getenv('RIPE_API')
-base_url = "https://atlas.ripe.net/api/v2/"
-probe_limit = 1000
+base_url = 'https://atlas.ripe.net/api/v2/'
+probe_limit = 100
 debug = True
+output_csv = 'atlas_measurements.csv'
+interesting_asns = 'interesting_asns'
 
 
 def create_measurements(probes, target):
@@ -51,13 +54,32 @@ def create_measurements(probes, target):
 def main():
 
     probes_by_as = load_json()
-    probes = [p[0] for (a,p) in probes_by_as.items()][:probe_limit]
-    probe_ids = [p['id'] for p in probes]
+    probes = [p[0] for (a, p) in probes_by_as.items()]
+    with open(interesting_asns) as r:
+        asns = [int(l) for l in r.readlines()]
 
-    for target in probes:
-        #response = create_measurements(probe_ids, target)
-        #print(response)
-        break
+    new_probes = {}
+
+    for p in probes:
+        if p['asn_v6'] in asns:
+            new_probes[p['id']] = p
+    for p in random.sample(probes, len(probes)):
+        if len(new_probes) >= probe_limit:
+            break
+        if p['asn_v6'] not in asns:
+            new_probes[p['id']] = p
+
+    with open(output_csv, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['dst_probe_id', 'msm_id_v4', 'msm_id_v6'])
+        for target in new_probes.values():
+            response = create_measurements(list(new_probes.keys()), target)
+            if True in response:
+                print(response[1]['measurements'])
+                writer.writerow([target['id']] + response[1]['measurements'])
+            else:
+                print(response)
+
 
 if __name__ == '__main__':
     main()
